78fb8a86bb7ab76698c71a45357cf7f54d4aa685
EZEQUIEL IAIR BERAKLA <48715745@ort>
2024-07-02 08:59:47 -0300

    completamos consigna Models y Controllers

---
+++ TP5/Models/Escape.cs
@@ -0,0 +1,20 @@
+public static class Escape {
+    public static string incognitasSalas {get; set;}  
+    public static string estadoJuego {get; set;} 
+
+    private static void InicializarJuego()  {
+
+    }
+    public static int GetEstadoJuego()  {
+    
+        int Sala = 0;
+
+        return Sala;
+    }
+    public static bool ResolverSala(int Sala, string Incognita)  {
+
+        bool Resuelve = false;
+
+        return Resuelve;
+    }
+}
