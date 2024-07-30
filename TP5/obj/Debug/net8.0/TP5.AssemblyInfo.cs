eb5275564540cb8fd48f8fa9550744f09d63f5d3
PEDRO GERMAN GARCIA <48315562@ort>
2024-07-30 08:59:31 -0300

    Trabajo de hoy de Pedro

--- TP5/obj/Debug/net8.0/TP5.AssemblyInfo.cs
+++ TP5/obj/Debug/net8.0/TP5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c02f8b9721a3bb154465b553824245e2faeb5d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d44346fabe175c79f72fede4d0c17251701d6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
